dc01a24a68beb6824f1afc7e2cdca62fb7f883ca
kehuan <semapwang@gmail.com>
2020-01-18 11:27:49 -0800

    Generate MakeTransfer and RecentTransactions components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MakeTransferComponent } from './make-transfer/make-transfer.component';
+import { RecentTransactionsComponent } from './recent-transactions/recent-transactions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MakeTransferComponent,
+    RecentTransactionsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/make-transfer/make-transfer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-make-transfer',
+  templateUrl: './make-transfer.component.html',
+  styleUrls: ['./make-transfer.component.scss']
+})
+export class MakeTransferComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
