54bcab9266fc97971960ab02bfbf6789de3949c8
kehuan <semapwang@gmail.com>
2020-01-18 21:18:07 -0800

    Filter transactions

--- src/app/recent-transactions/recent-transactions.component.ts
+++ src/app/recent-transactions/recent-transactions.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {TransferService} from '../services/transfer.service';
+import {FormControl} from '@angular/forms';
+import {combineLatest, Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {Transaction} from '../services/entities';
 
 @Component({
   selector: 'app-recent-transactions',
@@ -7,12 +11,26 @@ import {TransferService} from '../services/transfer.service';
   styleUrls: ['./recent-transactions.component.scss']
 })
 export class RecentTransactionsComponent implements OnInit {
+  searchText: FormControl = new FormControl('');
 
-  constructor(private transferService: TransferService) { }
+  readonly transactions$ = combineLatest(
+      this.transferService.transactions$,
+      this.searchText.valueChanges.pipe(startWith('')))
+    .pipe(
+      map(([transactions, text]) => this.filterTransactions(transactions, text))
+    );
 
-  get transactions$() {
-    return this.transferService.transactions$;
-  }
+  constructor(private transferService: TransferService) {}
+
   ngOnInit() {
+  }
+  //
+  private filterTransactions(transactions: Transaction[], text: string): Transaction[] {
+    if (!text) { return transactions; }
+
+    const regex = new RegExp(`${text}`, 'i');
+    return transactions.filter(
+      transaction => regex.test(transaction.merchant) || regex.test(transaction.transactionType)
+    );
   }
 }
